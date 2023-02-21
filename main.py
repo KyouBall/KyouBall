b4af3fa9278dcdf0ab986c56738bef810d43227b
Shigure <tokiamekyo@gmail.com>
2023-02-22 01:04:45 +0800

    adapt for itchat-uos 1.5.0-dev

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import itchat
+from itchat.content import TEXT
 
-@itchat.msg_register(itchat.content.TEXT)
+
+@itchat.msg_register(TEXT)
 def print_content(msg):
     print(msg['Text'])
+
 
 itchat.auto_login()
 itchat.run()
